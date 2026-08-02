c00a511f74b1abb5d1eb367ef1c03876ce17e50e
Сергей <smuki081991@gmail.com>
2026-08-02 16:38:04 +0300

    feat: реализация README.md

--- src/test/java/ru/mentee/power/devtools/progress/ProgressLoopTest.java
+++ src/test/java/ru/mentee/power/devtools/progress/ProgressLoopTest.java
@@ -1,10 +1,10 @@
 package ru.mentee.power.devtools.progress;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Тестирование ProgressTracker")
 class ProgressLoopTest {
