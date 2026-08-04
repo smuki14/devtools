4f756f4f477cbd6af7393c15070a74a93ff54bce
Сергей <smuki081991@gmail.com>
2026-08-04 19:45:10 +0300

    fix: исправлен порядок импортов в тестах

--- src/test/java/ru/mentee/power/devtools/progress/ProgressLoopTest.java
+++ src/test/java/ru/mentee/power/devtools/progress/ProgressLoopTest.java
@@ -1,11 +1,11 @@
 package ru.mentee.power.devtools.progress;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- src/test/java/ru/mentee/power/devtools/student/StudentListTest.java
+++ src/test/java/ru/mentee/power/devtools/student/StudentListTest.java
@@ -1,6 +1,7 @@
 package ru.mentee.power.devtools.student;
 
 import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
