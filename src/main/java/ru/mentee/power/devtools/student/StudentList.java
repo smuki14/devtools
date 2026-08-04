ae799c751b4a986834370a84fd2345bf574d10dc
Сергей <smuki081991@gmail.com>
2026-08-04 17:22:15 +0300

    feat: реализация readme и исправление ошибок studentlist в feature/dvt-11

--- src/main/java/ru/mentee/power/devtools/student/StudentList.java
+++ src/main/java/ru/mentee/power/devtools/student/StudentList.java
@@ -7,29 +7,29 @@ import java.util.stream.Collectors;
 public class StudentList {
     private final List<Student> students = new ArrayList<>();
 
-    public void addStudent (Student student) {
+    public void addStudent(Student student) {
+        if (student == null) {
+            throw new IllegalArgumentException("Student cannot be null");
+        }
         students.add(student);
     }
 
     public int size() {
-
         return students.size();
     }
 
     public List<Student> getAll() {
-
         return new ArrayList<>(students);
     }
 
     public List<Student> getStudentsByCity(String city) {
-
         if (city == null || city.isEmpty()) {
             return new ArrayList<>();
         }
 
 
         return students.stream()
-                .filter(s -> s.getCity() != null && s.getCity().equals(city))
-                .collect(Collectors.toList());
+                .filter(s -> s.getCity() != null && s.getCity().equalsIgnoreCase(city))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), List::copyOf));
     }
 }
