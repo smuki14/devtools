f00463db57f8934052602efb376c2989beacdb2d
Сергей <smuki081991@gmail.com>
2026-08-04 20:25:38 +0300

    fix: исправлен порядок импортов в тестах

--- src/test/java/ru/mentee/power/devtools/student/StudentListTest.java
+++ src/test/java/ru/mentee/power/devtools/student/StudentListTest.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 class StudentListTest {
 
